6d91102939cda25e5b3c3b245d821f564b03b762
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-09 11:19:37 -0300

    Header: add directory dist.

---
+++ dist/components/htmlElements.js
@@ -0,0 +1,8 @@
+export const sandwichMenu = document.getElementById('header__menu-sandwich-id');
+export const menuElementsContainer = document.getElementById('header__nav-container-id');
+export const inputRadio1 = document.getElementById('nav__input-radio-id-1');
+export const inputRadio2 = document.getElementById('nav__input-radio-id-2');
+export const inputRadio3 = document.getElementById('nav__input-radio-id-3');
+export const inputRadio4 = document.getElementById('nav__input-radio-id-4');
+export const inputRadio5 = document.getElementById('nav__input-radio-id-5');
+export const header = document.getElementById('header-id');
