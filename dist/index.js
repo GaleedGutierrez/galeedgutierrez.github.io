26056efbdb0f14ec1b3ae28c61dff7f165c92ca9
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-10 01:03:22 -0300

    Agregado google analytics.

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,19 @@ import { sandwichMenu, menuElementsContainer } from "./components/htmlElements.j
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: Unreachable code error
 import Typed from "../dist/components/typed.js/src/typed.js";
-var showMenu = function () {
+const showMenu = () => {
     menuElementsContainer.classList.toggle('visibleHeaderMenu');
     sandwichMenu.classList.toggle('header__menu-sandwich--active');
 };
-var hiddeMenu = function () {
+const hiddeMenu = () => {
     menuElementsContainer.classList.remove('visibleHeaderMenu');
     sandwichMenu.classList.remove('header__menu-sandwich--active');
 };
-sandwichMenu.onclick = function () { return showMenu(); };
-menuElementsContainer.onclick = function () { return hiddeMenu(); };
+sandwichMenu.onclick = () => showMenu();
+menuElementsContainer.onclick = () => hiddeMenu();
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-var typed = new Typed('.home__my-name', {
+const typed = new Typed('.home__my-name', {
     strings: ['Galeed Gutierrez'],
     startDelay: 500,
-    typeSpeed: 60
+    typeSpeed: 60,
 });
