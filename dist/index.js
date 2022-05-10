abfc80fc9fef73f41ed0d297992e872e5b9f2934
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-10 00:01:12 -0300

    Home: Ready.

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,20 @@
 import { sandwichMenu, menuElementsContainer } from "./components/htmlElements.js";
-const showMenu = () => {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: Unreachable code error
+import Typed from "../node_modules_me/typed.js/src/typed.js";
+var showMenu = function () {
     menuElementsContainer.classList.toggle('visibleHeaderMenu');
     sandwichMenu.classList.toggle('header__menu-sandwich--active');
 };
-const hiddeMenu = () => {
+var hiddeMenu = function () {
     menuElementsContainer.classList.remove('visibleHeaderMenu');
     sandwichMenu.classList.remove('header__menu-sandwich--active');
 };
-sandwichMenu.onclick = () => showMenu();
-menuElementsContainer.onclick = () => hiddeMenu();
+sandwichMenu.onclick = function () { return showMenu(); };
+menuElementsContainer.onclick = function () { return hiddeMenu(); };
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var typed = new Typed('.home__my-name', {
+    strings: ['Galeed Gutierrez'],
+    startDelay: 500,
+    typeSpeed: 60
+});
