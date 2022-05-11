a1d91b68b3972c955aadaf3aa5cf2e7d440c46b2
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-11 15:31:08 -0300

    Sección about terminada.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
-import { sandwichMenu, menuElementsContainer} from "./components/htmlElements.js";
+import { sandwichMenu, menuElementsContainer, inputHomeSandwichMenu, aHomeSandwichMenu, aAboutSandwichMenu, aProyectsSandwichMenu, aResumeSandwichMenu, aContactSandwichMenu, inputAboutSandwichMenu, inputProyectsSandwichMenu, inputResumeSandwichMenu, inputContactSandwichMenu} from "./components/htmlElements.js";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: Unreachable code error
 import Typed from "../dist/components/typed.js/src/typed.js";
-
-const showMenu = () => {
-    menuElementsContainer.classList.toggle('visibleHeaderMenu');
-    sandwichMenu.classList.toggle('header__menu-sandwich--active');
-};
-
-const hiddeMenu = () => {
-    menuElementsContainer.classList.remove('visibleHeaderMenu');
-    sandwichMenu.classList.remove('header__menu-sandwich--active');
-};
-
-sandwichMenu.onclick = () => showMenu();
-
-menuElementsContainer.onclick = () => hiddeMenu();
+import { objInputRadioType } from "./components/types.js";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const typed = new Typed('.home__my-name', {
@@ -24,3 +11,31 @@ const typed = new Typed('.home__my-name', {
     typeSpeed: 60,
 });
 
+const showMenu = (): void => {
+    menuElementsContainer.classList.toggle('visibleHeaderMenu');
+    sandwichMenu.classList.toggle('header__menu-sandwich--active');
+};
+
+const hiddeMenu = (): void => {
+    menuElementsContainer.classList.remove('visibleHeaderMenu');
+    sandwichMenu.classList.remove('header__menu-sandwich--active');
+};
+
+const addBrackets = (type: string): void => {
+    const objInputs: objInputRadioType = {
+        'home': () => inputHomeSandwichMenu.checked = true,
+        'about': () => inputAboutSandwichMenu.checked = true,
+        'proyects': () => inputProyectsSandwichMenu.checked = true,
+        'resume': () => inputResumeSandwichMenu.checked = true,
+        'contact': () => inputContactSandwichMenu.checked = true,
+    };
+    objInputs[type]();
+};
+
+sandwichMenu.onclick = () => showMenu();
+menuElementsContainer.onclick = () => hiddeMenu();
+aHomeSandwichMenu.onclick = () => addBrackets('home');
+aAboutSandwichMenu.onclick = () => addBrackets('about');
+aProyectsSandwichMenu.onclick = () => addBrackets('proyects');
+aResumeSandwichMenu.onclick = () => addBrackets('resume');
+aContactSandwichMenu.onclick = () => addBrackets('contact');
