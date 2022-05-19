660ad495fbc62e7dc57b79adb4c904b8b8f4adc2
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-18 21:52:54 -0300

    Sección de proyectos terminada.

--- src/components/interfaces.ts
+++ src/components/interfaces.ts
@@ -1,19 +1,19 @@
 export interface Proyect {
-    id:           number;
-    name:         string;
+    id: number;
+    name: string;
     technologies: string[];
-    repository:   string;
-    url:          string[];
-    img:          string;
-    date:         Date;
-    course:       CoursePlataform;
-    description:  string;
-    author:       string;
+    repository: string;
+    url: string[] | string;
+    img: string;
+    date: Date;
+    course: CoursePlataform;
+    description: string;
+    author: string;
 }
 
 export interface CoursePlataform {
-    name:     string;
+    name: string;
     platform: string;
-    url:      string;
-    img:      string;
+    url: string;
+    img: string;
 }
