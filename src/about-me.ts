cfb3a031f8d7968557332db923af275ecc2db5ad
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-06-22 23:49:08 -0300

    :lipstick: Estilos del footer terminado

--- src/about-me.ts
+++ src/about-me.ts
@@ -1,6 +1,9 @@
-import { BUTTON_MORE_ABOUT_ME, TEXT_MORE_ABOUT_ME } from './components/htmlElements';
+import {
+	BUTTON_MORE_ABOUT_ME,
+	TEXT_MORE_ABOUT_ME,
+} from './components/htmlElements';
 
-const originalDates = (): () => number => {
+const originalDates = (): (() => number) => {
 	let inicialNumber = 0;
 
 	return function (): number {
@@ -12,9 +15,8 @@ const originalDates = (): () => number => {
 
 const showMoreAboutMe = (): void => {
 	TEXT_MORE_ABOUT_ME.classList.toggle('visible-more-about-me');
-	BUTTON_MORE_ABOUT_ME.innerText = (condition() % 2 === 0)
-		? 'Leer más'
-		: 'Leer menos';
+	BUTTON_MORE_ABOUT_ME.innerText =
+		condition() % 2 === 0 ? 'Leer más' : 'Leer menos';
 };
 
 const condition = originalDates();
