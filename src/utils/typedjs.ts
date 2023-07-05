d81ec57edee2c4c63a8a7167531b3f1e6840816f
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-07-05 18:19:51 -0300

    :rocket: Deploy web :D

--- src/utils/typedjs.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const Typed = require('typed.js');
-
-// Typed.js is a library that types, in this case types "Galeed Gutierrez"
-new Typed('.g-home__my-name', {
-	strings: ['Galeed Gutierrez'],
-	startDelay: 500,
-	typeSpeed: 60,
-});
