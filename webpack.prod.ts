e2a2abd35d4e1364baa8243f05646566c1b58896
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-07-10 22:54:37 -0300

    :package: Eliminar CSS no usado con PurgeCSS

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import path from 'path';
 import TerserPlugin from 'terser-webpack-plugin';
 import { Configuration } from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
@@ -8,10 +9,20 @@ import { merge } from 'webpack-merge';
 // import { GenerateSW } from 'workbox-webpack-plugin';
 import common from './webpack.common';
 
+const glob = require('glob-all');
+
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+
+('purgecss-webpack-plugin');
+
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const IS_DEV = process.env.IS_DEV === 'true';
-
+const PATHS = {
+	public: path.join(__dirname, 'public'),
+	src: path.join(__dirname, 'src'),
+	styles: path.join(__dirname, 'styles'),
+};
 const config: Configuration = {
 	mode: 'production',
 	optimization: {
@@ -36,6 +47,11 @@ const config: Configuration = {
 		hints: 'warning',
 	},
 	plugins: [
+		new PurgeCSSPlugin({
+			paths: glob.sync([`${PATHS.public}/**/*`, `${PATHS.src}/**/*`], {
+				nodir: true,
+			}),
+		}),
 		// new MiniCssExtractPlugin({
 		// 	filename: '[name].css',
 		// }),
