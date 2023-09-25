1cc501b519ea39f4009242755749195019971d9b
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-09-25 15:20:03 -0300

    :lipstick: Se cambiaron los fill de los SVG por currentcolor

--- src/utils/menu.ts
+++ src/utils/menu.ts
@@ -4,6 +4,7 @@ import {
 	MENU_ELEMENTS_CONTAINER,
 	OPTIONS_MENU_CONTAINER,
 	OPTIONS_MENU_ICON,
+	SANDWICH_MENU,
 	SANDWICH_MENU_ICON,
 	THEME_INPUT,
 } from '@utils/nodes';
@@ -15,6 +16,7 @@ export const showMenu = (): void => {
 		'g-header__menu-container--visible'
 	);
 	SANDWICH_MENU_ICON.classList.toggle('a-sandwich-menu--active');
+	SANDWICH_MENU.classList.toggle('a-sandwich-menu-button--active');
 
 	const IS_VISIBLE_MENU = MENU_ELEMENTS_CONTAINER.classList.contains(
 		'g-header__menu-container--visible'
